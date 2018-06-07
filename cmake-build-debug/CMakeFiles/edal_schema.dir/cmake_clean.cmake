4932a09989956933410a4d7b890023600b382d7b
carlschiller <carl.schiller@gmail.com>
2018-06-07 17:09:01 +0200

    Added clion files

---
+++ cmake-build-debug/CMakeFiles/edal_schema.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/edal_schema.dir/main.cpp.o"
+  "edal_schema.pdb"
+  "edal_schema"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/edal_schema.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/edal_schema.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for edal_schema.
+# This may be replaced when dependencies are built.
